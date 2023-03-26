335310c2217279068cf58aba0080dd2cf667c9e7
Tarık Yıldız <112180001+tarik-yildiz@users.noreply.github.com>
2023-03-26 14:50:54 +0300

    signup activity identifying objects in source code

--- app/src/main/java/com/yildiztarik/stegochat/SignUpActivity.java
+++ app/src/main/java/com/yildiztarik/stegochat/SignUpActivity.java
@@ -1,14 +1,25 @@
 package com.yildiztarik.stegochat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    EditText editTextUserName,editTextPassword,editTextName;
+    ConstraintLayout constraintLayoutSignIn,constraintLayoutSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        initComponents();
+    }
+    private void initComponents(){
+        editTextName=findViewById(R.id.editTextNameRegister);
+        editTextUserName=findViewById(R.id.editTextUserNameRegister);
+        editTextPassword=findViewById(R.id.editTextPasswordRegister);
+        constraintLayoutSignIn=findViewById(R.id.constraintLayoutSignInRegister);
+        constraintLayoutSignUp=findViewById(R.id.constraintLayoutSignUpRegister);
     }
 }
