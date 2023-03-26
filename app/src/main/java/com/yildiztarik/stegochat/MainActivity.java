649b1b9a13245b1ae936b67389ee28374687955d
Tarık Yıldız <112180001+tarik-yildiz@users.noreply.github.com>
2023-03-26 14:48:59 +0300

    sign in activity identifying elements in source code

--- app/src/main/java/com/yildiztarik/stegochat/MainActivity.java
+++ app/src/main/java/com/yildiztarik/stegochat/MainActivity.java
@@ -1,14 +1,25 @@
 package com.yildiztarik.stegochat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editTextUserName,editTextPassword;
+    ConstraintLayout constraintLayoutSignIn,constraintLayoutSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initComponents();
+    }
+
+    private void initComponents(){
+        editTextUserName=findViewById(R.id.editTextLUserNameLogin);
+        editTextPassword=findViewById(R.id.editTextPasswordLogin);
+        constraintLayoutSignIn=findViewById(R.id.constraintLayoutSignInLogin);
+        constraintLayoutSignUp=findViewById(R.id.constraintLayoutSignUpLogin);
     }
 }
