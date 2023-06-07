f29895a685d3a375c7cdda8026c2e679f2a8ef9b
Tarık Yıldız <112180001+tarik-yildiz@users.noreply.github.com>
2023-06-07 22:04:56 +0300

    random username added when register

--- app/src/main/java/com/yildiztarik/stegochat/Activities/SignUpActivity.java
+++ app/src/main/java/com/yildiztarik/stegochat/Activities/SignUpActivity.java
@@ -1,4 +1,5 @@
-package com.yildiztarik.stegochat;
+package com.yildiztarik.stegochat.Activities;
+
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,14 +14,26 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.yildiztarik.stegochat.R;
+import com.yildiztarik.stegochat.Utils.EmailValidator;
+import com.yildiztarik.stegochat.Utils.ActivityManager;
+import com.yildiztarik.stegochat.Utils.PasswordValidator;
+import com.yildiztarik.stegochat.Utils.RandomUsernameGenerator;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
-
     FirebaseAuth auth;
+    FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
     EditText editTextMail, editTextPassword;
     ConstraintLayout constraintLayoutSignIn, constraintLayoutSignUp;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,17 +41,15 @@ public class SignUpActivity extends AppCompatActivity {
         initComponents();
         methodOnClick();
     }
-
     private void initComponents() {
         editTextMail = findViewById(R.id.editTextMailRegister);
         editTextPassword = findViewById(R.id.editTextPasswordRegister);
-
         constraintLayoutSignIn = findViewById(R.id.constraintLayoutSignInRegister);
         constraintLayoutSignUp = findViewById(R.id.constraintLayoutSignUpRegister);
 
         auth = FirebaseAuth.getInstance();
+        user=auth.getCurrentUser();
     }
-
     public void methodOnClick() {
         constraintLayoutSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,16 +57,25 @@ public class SignUpActivity extends AppCompatActivity {
 
                 String mail = editTextMail.getText().toString();
                 String password = editTextPassword.getText().toString();
+                PasswordValidator passwordValidator=new PasswordValidator(password);
                 if (mail.isEmpty() || password.isEmpty()) {
                     Toast.makeText(SignUpActivity.this, "username and password required", Toast.LENGTH_SHORT).show();
-                }else if(password.length()<=5){
-                    Toast.makeText(SignUpActivity.this, "password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+                }else if(!passwordValidator.isValid()){
+                    String passwordConditions="The password must be 8 characters long and should contain both uppercase and lowercase letters as well as numbers.";
+                    Toast.makeText(SignUpActivity.this, passwordConditions, Toast.LENGTH_SHORT).show();
                 }else if(!EmailValidator.validate(mail)){
                     Toast.makeText(SignUpActivity.this, "Email format is not valid!", Toast.LENGTH_SHORT).show();
                 }
                 else {
                     register(mail, password);
                 }
+            }
+        });
+
+        constraintLayoutSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityManager.getInstance(SignUpActivity.this).startActivity(MainActivity.class);
             }
         });
     }
@@ -65,11 +85,20 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
-                    Toast.makeText(SignUpActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                    firebaseDatabase=FirebaseDatabase.getInstance();
+                    databaseReference =firebaseDatabase.getReference().child("Users").child(auth.getUid());
+                    RandomUsernameGenerator usernameGenerator=new RandomUsernameGenerator();
+
+                    Map map =new HashMap();
+                    map.put("username",usernameGenerator.generateUsername());
+                    databaseReference.setValue(map);
+                    Toast.makeText(SignUpActivity.this, "Sign up Success!", Toast.LENGTH_SHORT).show();
+                    ActivityManager.getInstance(SignUpActivity.this).startActivity(UserLoggedIn.class);
                 } else {
-                    Toast.makeText(SignUpActivity.this, "Fail!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SignUpActivity.this, "Failed to sign up!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
+
 }
